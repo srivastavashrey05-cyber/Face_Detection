21af1c5b346e85ea189299d1f49e07ddaf9f694c
Shreyansh Srivastava <srivastavashrey05@gmail.com>
2025-08-28 15:59:31 +0530

    Add files via upload

---
+++ face_detection.py
@@ -0,0 +1,24 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+webcam = cv2.VideoCapture(0)
+
+while True:
+    _, img = webcam.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.5, 4)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
+    
+    cv2.putText(img, f"Faces detected: {len(faces)}", (10, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2, cv2.LINE_AA)
+
+    cv2.imshow("Face Detection", img)
+    key = cv2.waitKey(10)
+    if key == 27:  # ESC to exit
+        break
+
+webcam.release()
+cv2.destroyAllWindows()
